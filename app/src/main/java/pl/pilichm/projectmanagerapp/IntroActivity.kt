5f80c8765fe48678e1503cd290ef3a76708f2f11
pilichm <michal.pilichowski@onet.pl>
2021-08-21 16:12:12 +0200

    Added: - sign in/sign up activity, - moving from splash screen activity to sign in activity after 2.5 second delay.

--- app/src/main/java/pl/pilichm/projectmanagerapp/IntroActivity.kt
+++ app/src/main/java/pl/pilichm/projectmanagerapp/IntroActivity.kt
@@ -1,24 +1,19 @@
 package pl.pilichm.projectmanagerapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.WindowManager
 
-class SplashActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_intro)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 2500)
+
     }
 }
--- app/src/main/java/pl/pilichm/projectmanagerapp/MainActivity.kt
+++ app/src/main/java/pl/pilichm/projectmanagerapp/MainActivity.kt
@@ -8,12 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-
     }
 }
