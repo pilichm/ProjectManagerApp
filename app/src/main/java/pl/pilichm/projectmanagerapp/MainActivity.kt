66c2b58429e903afa00901e5551fd60eb0569ce8
pilichm <michal.pilichowski@onet.pl>
2021-08-21 15:53:54 +0200

    Initial commit: - app creation, - added splash screen with app name.

---
+++ app/src/main/java/pl/pilichm/projectmanagerapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package pl.pilichm.projectmanagerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+    }
+}
